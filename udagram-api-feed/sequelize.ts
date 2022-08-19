46b8f946d7e6c4b08cb25ce8e2684e7ef3b54578
Joresti <j.oresti@gmail.com>
2022-08-19 11:50:06 -0400

    trying to debug deployment

--- udagram-api-feed/sequelize.ts
+++ udagram-api-feed/sequelize.ts
@@ -2,12 +2,12 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './src/config/config';
 
 console.log("config", config)
-export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+// export const sequelize = new Sequelize({
+//   'username': config.username,
+//   'password': config.password,
+//   'database': config.database,
+//   'host': config.host,
 
-  'dialect': config.dialect,
-  'storage': ':memory:',
-});
+//   'dialect': config.dialect,
+//   'storage': ':memory:',
+// });
