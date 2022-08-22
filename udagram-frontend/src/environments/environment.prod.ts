56d7929a1c72546dfa40e5f7252bd9eb58c8f6d4
Joresti <j.oresti@gmail.com>
2022-08-22 11:47:34 -0400

    ready to deploy

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a843df3fd05d74886a5b4586262f6be3-2029194068.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
